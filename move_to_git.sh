50fc32b1877ab83f280c1d4ed2561019acff5c6a
Sumitro Bhaumik <sumitros@gmail.com>
2017-06-22 02:20:16 +0530

    New update

--- move_to_git.sh
+++ move_to_git.sh
@@ -1,27 +1,28 @@
+#all variables
 deploy_dir="../thexavier666.github.io/"
-hugo_dir="./public/"
+html_dir="./public/"
 all_files="*"
-git_file='do_git.sh'
+git_file="do_git.sh"
 
 # Removing previous files
+echo "Removing previous files"
 rm -rf $deploy_dir$all_files
-
-read -n 1 -s
+#read -n 1 -s
 
 # Moving new files to deployment folder
-cp -r $hugo_dirs$all_files $deploy_dir
+echo "Moving new files to deployment folder"
+cp -r $html_dir$all_files $deploy_dir
+#read -n 1 -s
 
-read -n 1 -s
-
-# Adding permission to git script
-chmod +x $git_file
-
-read -n 1 -s
-
-# Copying the file to deployment folder
+# Copying the gitting file to deployment folder
+# Since it was deleted last time
+echo "Copying the file to deployment folder"
 cp $git_file $deploy_dir
-
-read -n 1 -s
+#read -n 1 -s
 
 # Creating new shell | changing to deployment dir | gitting
+echo "Creating new shell | changing to deployment dir | gitting"
 (cd $deploy_dir && exec sh $git_file)
+
+# gitting the MD files as well
+sh $git_file
